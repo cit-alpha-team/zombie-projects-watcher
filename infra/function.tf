465f8cd78ec86ea767d985e00f00f8ef10d25754
acustodio <acustodio@ciandt.com>
2025-08-18 09:43:59 -0300

    style(terraform): Format configuration files with terraform fmt

--- infra/function.tf
+++ infra/function.tf
@@ -2,16 +2,16 @@ data "archive_file" "source" {
   type        = "zip"
   source_dir  = "${path.module}/../"
   output_path = "/tmp/zombie-watcher-source.zip"
-  excludes    = [
+  excludes = [
     "terraform/",
     ".git/",
     "*.tfvars"
-    ]
+  ]
 }
 
 resource "google_storage_bucket" "function_source_bucket" {
-  name     = "${var.project_id}-gcf-source"
-  location = var.region
+  name                        = "${var.project_id}-gcf-source"
+  location                    = var.region
   uniform_bucket_level_access = true
 }
 
@@ -41,10 +41,10 @@ resource "google_cloudfunctions2_function" "zombie_watcher_function" {
   }
 
   service_config {
-    max_instance_count = 1
-    min_instance_count = 0
-    available_memory   = "256Mi"
-    timeout_seconds    = 540
+    max_instance_count    = 1
+    min_instance_count    = 0
+    available_memory      = "256Mi"
+    timeout_seconds       = 540
     service_account_email = var.service_account_email
 
     environment_variables = {
@@ -59,7 +59,7 @@ resource "google_cloud_scheduler_job" "zombie_watcher_trigger" {
   description = "Aciona o Zombie Projects Watcher diariamente."
 
   http_target {
-    uri = google_cloudfunctions2_function.zombie_watcher_function.service_config[0].uri
+    uri         = google_cloudfunctions2_function.zombie_watcher_function.service_config[0].uri
     http_method = "POST"
 
     oidc_token {
